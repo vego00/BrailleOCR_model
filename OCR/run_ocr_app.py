cf1c051df612adac7753e59f08c79a212632a811
khmute <khmute@gmail.com>
2024-12-03 22:45:35 +0900

    update

--- OCR/run_ocr_app.py
+++ OCR/run_ocr_app.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python
 # coding: utf-8
 import os
-import boto3
-
 from pathlib import Path
 
 import OCR.local_config as local_config
 import OCR.model.infer_retinanet as infer_retinanet
 
-try:
-    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-except KeyError:
-    aws_access_key_id = None
-    print('AWS_ACCESS_KEY_ID not found in environment variables')
+# try:
+#     aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+# except KeyError:
+#     aws_access_key_id = None
+#     print('AWS_ACCESS_KEY_ID not found in environment variables')
 
-try:
-    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-except KeyError:
-    aws_secret_access_key = None
-    print('AWS_SECRET_ACCESS_KEY not found in environment variables')
+# try:
+#     aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+# except KeyError:
+#     aws_secret_access_key = None
+#     print('AWS_SECRET_ACCESS_KEY not found in environment variables')
     
-try:
-    bucket_name = os.environ['BUCKET_NAME']
-except KeyError:
-    bucket_name = None
-    print('BUCKET_NAME not found in environment variables')
+# try:
+#     bucket_name = os.environ['BUCKET_NAME']
+# except KeyError:
+#     bucket_name = None
+#     print('BUCKET_NAME not found in environment variables')
     
-try:
-    file_key = os.environ['FILE_KEY']
-except KeyError:
-    file_key = None
-    print('FILE_KEY not found in environment variables')
+# try:
+#     file_key = os.environ['FILE_KEY']
+# except KeyError:
+#     file_key = None
+#     print('FILE_KEY not found in environment variables')
 
-try:
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name='ap-northeast-2'
-    )
-except Exception as e:
-    print('Failed to create s3 client:', e)
+# try:
+#     s3 = boto3.client(
+#         's3',
+#         aws_access_key_id=aws_access_key_id,
+#         aws_secret_access_key=aws_secret_access_key,
+#         region_name='ap-northeast-2'
+#     )
+# except Exception as e:
+#     print('Failed to create s3 client:', e)
 
-model_weights = 'model.t7'
+# model_weights = 'model.t7'
 # try:
 #     with open(local_model_path, 'wb') as f:
 #         s3.download_fileobj(bucket_name, file_key, f)
@@ -57,7 +55,11 @@ model_weights = 'model.t7'
 
 def run_ocr(image_file):
     results_dir = local_config.data_path
-
+    recognizer = infer_retinanet.BrailleInference(
+        params_fn=os.path.join(local_config.data_path, 'weights', 'param.txt'),
+        model_weights_fn=os.path.join(local_config.data_path, 'weights', 'model.t7'),
+        create_script=None
+    )
     return recognizer.run_and_save(image_file, results_dir, target_stem=None,
                                                lang='RU', extra_info=None,
                                                draw_refined=recognizer.DRAW_NONE,
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://3.38.213.235/ocr'
+url = 'http://3.38.213.235:5000/ocr'
 files = {'image': open('data/서울사랑/서울사랑01.jpg', 'rb')}
 
 response = requests.post(url, files=files)
