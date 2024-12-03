ee2d1353aedb8736c4568ad2270ba4944d2f65f9
khmute <khmute@gmail.com>
2024-12-04 00:52:36 +0900

    update

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,10 +12,6 @@ apt-get update
 
 apt-get install -y python3.8 python3.8-venv python3.8-dev python3-pip
 
-# 'python3' 심볼릭 링크 변경을 제거합니다.
-# echo "Setting python3 to point to python3.8..."
-# update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
-
 echo "Creating /home/ubuntu/app directory if it doesn't exist..."
 mkdir -p /home/ubuntu/app
 
